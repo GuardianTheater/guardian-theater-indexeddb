8bf9c6a06554f8337f3d561f2c9223470026c803
Chris Fried <fried.chris@gmail.com>
2021-12-22 22:04:22 -0600

    env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
-import { bungieProd, twitchProd } from './keys';
+import { bungieProd, twitchProd, xboxProd } from './keys'
 
 export const environment = {
   production: true,
   bungie: bungieProd,
   twitch: twitchProd,
-};
+  xbox: xboxProd,
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,22 @@
 import { bungieDev, twitchDev, xboxDev } from './keys'
 
-export const environment = {
+export const environment: {
+  production: boolean
+  bungie: {
+    apiKey: string
+    clientId: string
+    redirect: string
+  }
+  twitch: {
+    clientId: string
+    redirect: string
+    parent: string
+  }
+  xbox: {
+    redirect: string
+    clientId: string
+  }
+} = {
   production: false,
   bungie: bungieDev,
   twitch: twitchDev,
